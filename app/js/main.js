bdb55ad9ead89b84ef538b0e307f734168f2592a
Meyirman <sarsenbaymeyirman@gmail.com>
2024-01-04 21:09:41 +0600

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function(){
+    
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+    });
     var mixer = mixitup('.products__inner-box');
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToScroll: 4,
+        slidesToShow: 4,
+    });
 });
